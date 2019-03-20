997d3c203c486badfca37885cb9a9b1990da8eb2
Peter Bell <peter.bell@trapezegroup.com>
2019-03-20 06:42:05 +0000

    Total Cost of expense line being automatically set

--- app/javascript/controllers/expenses_controller.js
+++ app/javascript/controllers/expenses_controller.js
@@ -1,13 +1,17 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-    static targets = [ "category", "vat", "unitCost" ];
+    static targets = [ "category", "vat", "qty", "unitCost", "totalCost" ];
 
+    // Event handler for when the user selects a particular expenses claim category
     categoryChange(event) {
-        console.log("Hello, categoryChange!", this.element);
-
-        let dataset = event.srcElement.selectedOptions[0].dataset
+        let dataset = event.srcElement.selectedOptions[0].dataset;
         this.vatTarget.value = dataset.vat;
         this.unitCostTarget.value = dataset.unitcost;
+    }
+
+    recalcClaim(event) {
+        console.log("Hello, recalcClaim!", this.element);
+        this.totalCostTarget.value = parseFloat(this.unitCostTarget.value) * parseInt(this.qtyTarget.value)
     }
 }
