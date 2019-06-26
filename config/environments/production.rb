95baf81a8fa0cd858034c476c31eb2f0443297c0
Peter Bell <peter.bell@trapezegroup.com>
2019-06-26 22:02:55 +0100

    Provided production configuration for mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,17 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  # Setup email server via MAILGUN.
+  ActionMailer::Base.smtp_settings = {
+      port: ENV['MAILGUN_SMTP_PORT'],
+      address: ENV['MAILGUN_SMTP_SERVER'],
+      user_name: ENV['MAILGUN_SMTP_LOGIN'],
+      password: ENV['MAILGUN_SMTP_PASSWORD'],
+      domain: "#{ENV['HEROKU_APP_NAME']}.herokuapp.com",
+      authentication: :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 
+  config.action_mailer.default_url_options = { host: 'tguk-jet.com' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
