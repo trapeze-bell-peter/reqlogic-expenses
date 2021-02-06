27dc3af89fc675be72e18bbe862ddea3f95794ae
Peter Bell <peter.bell@trapezegroup.com>
2021-02-06 19:40:30 +0000

    Javascript tidyups
    Adding semicolon to certain pages.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,9 @@
 // This file provides the standard Rails Javascript items.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
 Rails.start();
 Turbolinks.start();
